32ccf25a45dbb0c0ba3ceabd64dbdf97e9b12fac
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2010-11-23 23:16:55 -0700

    Much improved   - use multiple redis clients for the different things we do.   - pub/sub documents to analyze   - tighter timeouts   - faster return from the handler   - remove express   - gzip support to the backend

--- server.coffee
+++ server.coffee
@@ -1,35 +1,43 @@
 require('joose')
 require('joosex-namespace-depended')
 require('hash')
+fs = require('fs')
+assert = require('assert')
 queryString = require('querystring')
 http = require('http')
 sys = require('sys')
-httpProxy = require('./node-http-proxy')
+Url = require('url')
+httpProxy = require('./node-http-proxy') # https://github.com/nodejitsu/node-http-proxy
 jsdom = require('jsdom')
-express = require('express')
-redis = require('redis').createClient()
+Redis = require('redis') # https://github.com/mranney/node_redis
 request = require('request')
-jquery = 'https://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js'
-instapaper = 'www.instapaper.com'
 
-process = () ->
-  redis.lpop 'download', (error, reply) ->
+jquery = 'https://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js'
+sha1 = 'http://crypto-js.googlecode.com/files/2.0.0-crypto-sha1.js'
+instapaper = 'www.instapaper.com'
+shortestpaperScript = fs.readFileSync('shortestpaper.js', 'utf8')
+
+downloadClient = Redis.createClient()
+
+processDownloads = () ->
+  downloadClient.lpop 'download', (error, reply) ->
     if reply?
-      redis.llen 'download', (e, r) ->
+      downloadClient.llen 'download', (e, r) ->
         sys.puts("#{r} URLs left")
       url = reply.toString()
       key = Hash.sha1(url)[0..9]
-      redis.get key, (error, reply) ->
+      downloadClient.get key, (error, reply) ->
         unless reply?
           sys.puts("Analyzing #{url}")
-          request { uri: url }, (error, resp, body) ->
-            setTimeout(process, 10000)
+          uri = "http://#{instapaper}/text?u=#{escape(url)}"
+          request { uri: uri }, (error, resp, body) ->
+            setTimeout(processDownloads, 5000)
             unless error?
-              jsdom.jQueryify jsdom.jsdom(body).createWindow(), jquery, (w, $) ->
+              jsdom.jQueryify jsdom.jsdom(body, jsdom.defaultLevel, { url: uri }).createWindow(), jquery, (w, $) ->
                 if w.document.outerHTML.match(/Exceeded rate limit/)
                   # Try again
                   sys.puts("Retrying #{url}")
-                  redis.rpush 'download', url
+                  downloadClient.rpush 'download', url
                 else
                   # Count the number of words in the `#story` element.
                   count = 0
@@ -37,65 +45,72 @@ process = () ->
                   for word in words
                     # Only include words > 2 characters
                     count += 1 if word.length > 2
-                  redis.set key, count, (e, r) ->
+                  downloadClient.set key, count, (e, r) ->
                     sys.puts("Counted and stored #{url}")
         else
-          setTimeout(process, 1000)
+          setTimeout(processDownloads, 500)
     else
-      setTimeout(process, 1000)
+      setTimeout(processDownloads, 500)
 
-process()
+processDownloads()
+
+processorClient = Redis.createClient()
+pageProcessor = Redis.createClient()
+pageProcessor.on 'message', (channel, message) ->
+  message = JSON.parse(message)
+  jsdom.jQueryify jsdom.jsdom(message.buffer, jsdom.defaultLevel, { url: message.url }).createWindow(), jquery, (window, $) ->
+    document = window.document
+    $('#bookmark_list .tableViewCellTitleLink').each (i, e) ->
+      url = this.href
+      sys.puts(url)
+      key = Hash.sha1(url)[0..9]
+      sys.puts("Found URL #{url}")
+      processorClient.get key, (error, reply) ->
+        unless reply?
+          sys.puts("Queueing #{url} for download")
+          processorClient.rpush 'download', url
+
+pageProcessor.subscribe('processing')
+
+mainClient = Redis.createClient()
 
 httpProxy.createServer((req, res, proxy) ->
-  # Have to clear this so we don't get gzip crap
-  # TODO: Make it work with gzip
-  req.headers['accept-encoding'] = ''
-  buffer = ''
-  proxy.proxyRequest(80, instapaper, /text\/html/, ((chunk) ->
-    buffer += chunk
-  ), (() ->
-    jsdom.jQueryify jsdom.jsdom(buffer).createWindow(), jquery, (w, $) ->
-      document = w.document
-
-      script = document.createElement('script')
-      script.src = jquery
-      script.type = 'text/javascript'
-      document.body.appendChild(script)
-
-      script = document.createElement('script')
-      # script.src = "http://#{req.headers.host.replace(/8080/, '8081')}/sorting.js"
-      script.src = '/sorting.js'
-      script.type = 'text/javascript'
-      document.body.appendChild(script)
-
-      $('#bookmark_list > .tableViewCell').each (i, e) ->
-        url = "http://#{instapaper}#{$('.textButton', e).attr('href')}"
-        key = Hash.sha1(url)[0..9]
-        $(e).attr('key', key)
-        sys.puts("Found URL #{url}")
-        redis.get key, (error, reply) ->
-          unless reply?
-            sys.puts("Queueing #{url} for download")
-            redis.rpush 'download', url
-      res.end(w.document.outerHTML.replace(/&amp;/g, '&'))
-  ))
+  url = Url.parse(req.url)
+  switch url.pathname
+    when '/counts.json'
+      res.writeHead(200, {
+        'Content-Type': 'application/json; charset=utf-8'
+      })
+      query = queryString.parse(url.query)
+      params = query.keys.split(',')
+      params.push (error, reply) ->
+        counts = reply.toString('utf8').split(',')
+        h = {}
+        for i in [0..counts.length]
+          h[params[i]] = parseInt(counts[i])
+        res.end(JSON.stringify(h))
+      mainClient.mget.apply mainClient, params
+    when '/shortestpaper.js'
+      res.writeHead(200, {
+        'Content-Type': 'text/javascript; charset=utf-8'
+        'Content-Length': shortestpaperScript.length
+      })
+      res.end(shortestpaperScript)
+    else
+      gzip = !!url.pathname.match(/\.(js(on)?|js|jpe?g|png|gif|css)$/)
+      req.headers['accept-encoding'] = '' unless gzip
+      buffer = ''
+      proxy.proxyRequest(80, instapaper, /text\/html/, ((chunk) ->
+        chunk = chunk.toString('utf8')
+        buffer += chunk
+        res.write(chunk.replace(/<\/body>\s*<\/html>/, "<script type='text/javascript' src='#{jquery}'></script>\n<script type='text/javascript' src='/shortestpaper.js'></script>\n</body>\n</html>"))
+      ), (() ->
+        # gzip should be false
+        assert.equal(gzip, false)
+        res.end()
+        mainClient.publish('processing', JSON.stringify({
+          buffer: buffer,
+          url: "http://#{instapaper}#{req.url}"
+        }))
+      ))
 ).listen(8080)
-
-app = express.createServer()
-app.use(express.staticProvider(__dirname + '/public'))
-
-app.get '/counts.json', (req, res) ->
-  res.writeHead(200, {
-    'Content-Type': 'text/javascript; charset=utf-8'
-  })
-  query = queryString.parse(req.url.split('?')[1])
-  params = query.keys.split(',')
-  params.push (error, reply) ->
-    h = {}
-    counts = reply.toString('utf8').split(',')
-    for i in [0..counts.length]
-      h[params[i]] = parseInt(counts[i])
-    res.end("#{query.callback}(#{JSON.stringify(h)});");
-  redis.mget.apply redis, params
-
-app.listen(8081)
--- server.js
+++ server.js
@@ -1,38 +1,47 @@
-var app, express, http, httpProxy, instapaper, jquery, jsdom, process, queryString, redis, request, sys;
+var Redis, Url, assert, downloadClient, fs, http, httpProxy, instapaper, jquery, jsdom, mainClient, pageProcessor, processDownloads, processorClient, queryString, request, sha1, shortestpaperScript, sys;
 require('joose');
 require('joosex-namespace-depended');
 require('hash');
+fs = require('fs');
+assert = require('assert');
 queryString = require('querystring');
 http = require('http');
 sys = require('sys');
+Url = require('url');
 httpProxy = require('./node-http-proxy');
 jsdom = require('jsdom');
-express = require('express');
-redis = require('redis').createClient();
+Redis = require('redis');
 request = require('request');
 jquery = 'https://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js';
+sha1 = 'http://crypto-js.googlecode.com/files/2.0.0-crypto-sha1.js';
 instapaper = 'www.instapaper.com';
-process = function() {
-  return redis.lpop('download', function(error, reply) {
+shortestpaperScript = fs.readFileSync('shortestpaper.js', 'utf8');
+downloadClient = Redis.createClient();
+processDownloads = function() {
+  return downloadClient.lpop('download', function(error, reply) {
     var key, url;
     if (typeof reply !== "undefined" && reply !== null) {
-      redis.llen('download', function(e, r) {
+      downloadClient.llen('download', function(e, r) {
         return sys.puts("" + (r) + " URLs left");
       });
       url = reply.toString();
       key = Hash.sha1(url).slice(0, 9 + 1);
-      return redis.get(key, function(error, reply) {
+      return downloadClient.get(key, function(error, reply) {
+        var uri;
         if (!(typeof reply !== "undefined" && reply !== null)) {
           sys.puts("Analyzing " + (url));
+          uri = ("http://" + (instapaper) + "/text?u=" + (escape(url)));
           return request({
-            uri: url
+            uri: uri
           }, function(error, resp, body) {
-            setTimeout(process, 10000);
-            return !(typeof error !== "undefined" && error !== null) ? jsdom.jQueryify(jsdom.jsdom(body).createWindow(), jquery, function(w, $) {
+            setTimeout(processDownloads, 5000);
+            return !(typeof error !== "undefined" && error !== null) ? jsdom.jQueryify(jsdom.jsdom(body, jsdom.defaultLevel, {
+              url: uri
+            }).createWindow(), jquery, function(w, $) {
               var _i, _len, _ref, count, word, words;
               if (w.document.outerHTML.match(/Exceeded rate limit/)) {
                 sys.puts("Retrying " + (url));
-                return redis.rpush('download', url);
+                return downloadClient.rpush('download', url);
               } else {
                 count = 0;
                 words = $('#story').text().split(/\s+/);
@@ -43,76 +52,92 @@ process = function() {
                     count += 1;
                   }
                 }
-                return redis.set(key, count, function(e, r) {
+                return downloadClient.set(key, count, function(e, r) {
                   return sys.puts("Counted and stored " + (url));
                 });
               }
             }) : null;
           });
         } else {
-          return setTimeout(process, 1000);
+          return setTimeout(processDownloads, 500);
         }
       });
     } else {
-      return setTimeout(process, 1000);
+      return setTimeout(processDownloads, 500);
     }
   });
 };
-process();
-httpProxy.createServer(function(req, res, proxy) {
-  var buffer;
-  req.headers['accept-encoding'] = '';
-  buffer = '';
-  return proxy.proxyRequest(80, instapaper, /text\/html/, function(chunk) {
-    return buffer += chunk;
-  }, function() {
-    return jsdom.jQueryify(jsdom.jsdom(buffer).createWindow(), jquery, function(w, $) {
-      var document, script;
-      document = w.document;
-      script = document.createElement('script');
-      script.src = jquery;
-      script.type = 'text/javascript';
-      document.body.appendChild(script);
-      script = document.createElement('script');
-      script.src = '/sorting.js';
-      script.type = 'text/javascript';
-      document.body.appendChild(script);
-      $('#bookmark_list > .tableViewCell').each(function(i, e) {
-        var key, url;
-        url = ("http://" + (instapaper) + ($('.textButton', e).attr('href')));
-        key = Hash.sha1(url).slice(0, 9 + 1);
-        $(e).attr('key', key);
-        sys.puts("Found URL " + (url));
-        return redis.get(key, function(error, reply) {
-          if (!(typeof reply !== "undefined" && reply !== null)) {
-            sys.puts("Queueing " + (url) + " for download");
-            return redis.rpush('download', url);
-          }
-        });
+processDownloads();
+processorClient = Redis.createClient();
+pageProcessor = Redis.createClient();
+pageProcessor.on('message', function(channel, message) {
+  message = JSON.parse(message);
+  return jsdom.jQueryify(jsdom.jsdom(message.buffer, jsdom.defaultLevel, {
+    url: message.url
+  }).createWindow(), jquery, function(window, $) {
+    var document;
+    document = window.document;
+    return $('#bookmark_list .tableViewCellTitleLink').each(function(i, e) {
+      var key, url;
+      url = this.href;
+      sys.puts(url);
+      key = Hash.sha1(url).slice(0, 9 + 1);
+      sys.puts("Found URL " + (url));
+      return processorClient.get(key, function(error, reply) {
+        if (!(typeof reply !== "undefined" && reply !== null)) {
+          sys.puts("Queueing " + (url) + " for download");
+          return processorClient.rpush('download', url);
+        }
       });
-      return res.end(w.document.outerHTML.replace(/&amp;/g, '&'));
     });
   });
-}).listen(8080);
-app = express.createServer();
-app.use(express.staticProvider(__dirname + '/public'));
-app.get('/counts.json', function(req, res) {
-  var params, query;
-  res.writeHead(200, {
-    'Content-Type': 'text/javascript; charset=utf-8'
-  });
-  query = queryString.parse(req.url.split('?')[1]);
-  params = query.keys.split(',');
-  params.push(function(error, reply) {
-    var _ref, counts, h, i;
-    h = {};
-    counts = reply.toString('utf8').split(',');
-    _ref = counts.length;
-    for (i = 0; (0 <= _ref ? i <= _ref : i >= _ref); (0 <= _ref ? i += 1 : i -= 1)) {
-      h[params[i]] = parseInt(counts[i]);
-    }
-    return res.end("" + (query.callback) + "(" + (JSON.stringify(h)) + ");");
-  });
-  return redis.mget.apply(redis, params);
 });
-app.listen(8081);
+pageProcessor.subscribe('processing');
+mainClient = Redis.createClient();
+httpProxy.createServer(function(req, res, proxy) {
+  var buffer, gzip, params, query, url;
+  url = Url.parse(req.url);
+  switch (url.pathname) {
+    case '/counts.json':
+      res.writeHead(200, {
+        'Content-Type': 'application/json; charset=utf-8'
+      });
+      query = queryString.parse(url.query);
+      params = query.keys.split(',');
+      params.push(function(error, reply) {
+        var _ref, counts, h, i;
+        counts = reply.toString('utf8').split(',');
+        h = {};
+        _ref = counts.length;
+        for (i = 0; (0 <= _ref ? i <= _ref : i >= _ref); (0 <= _ref ? i += 1 : i -= 1)) {
+          h[params[i]] = parseInt(counts[i]);
+        }
+        return res.end(JSON.stringify(h));
+      });
+      return mainClient.mget.apply(mainClient, params);
+    case '/shortestpaper.js':
+      res.writeHead(200, {
+        'Content-Type': 'text/javascript; charset=utf-8',
+        'Content-Length': shortestpaperScript.length
+      });
+      return res.end(shortestpaperScript);
+    default:
+      gzip = !!url.pathname.match(/\.(js(on)?|js|jpe?g|png|gif|css)$/);
+      if (!(gzip)) {
+        req.headers['accept-encoding'] = '';
+      }
+      buffer = '';
+      return proxy.proxyRequest(80, instapaper, /text\/html/, function(chunk) {
+        chunk = chunk.toString('utf8');
+        buffer += chunk;
+        return res.write(chunk.replace(/<\/body>\s*<\/html>/, "<script type='text/javascript' src='" + (jquery) + "'></script>\n<script type='text/javascript' src='/shortestpaper.js'></script>\n</body>\n</html>"));
+      }, function() {
+        assert.equal(gzip, false);
+        res.end();
+        return mainClient.publish('processing', JSON.stringify({
+          buffer: buffer,
+          url: ("http://" + (instapaper) + (req.url))
+        }));
+      });
+  }
+}).listen(8080);
